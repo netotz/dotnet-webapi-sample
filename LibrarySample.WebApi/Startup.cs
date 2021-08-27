d751b8aa4d3e764c9f77e64e44c8c65249f4ebd4
netotz <neto.otz@hotmail.com>
2021-08-27 10:01:33 -0500

    Configure database context for MySQL

--- LibrarySample.WebApi/Startup.cs
+++ LibrarySample.WebApi/Startup.cs
@@ -1,6 +1,9 @@
 
+using LibrarySample.WebApi.Database;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -9,7 +12,6 @@ using Microsoft.OpenApi.Models;
 namespace LibrarySample.WebApi {
     public class Startup {
         public IConfiguration Configuration { get; }
-
         public IWebHostEnvironment Environment { get; }
 
         public Startup(IConfiguration configuration, IWebHostEnvironment environment) {
@@ -19,10 +21,18 @@ namespace LibrarySample.WebApi {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
-
             services.AddControllers();
             services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "LibrarySample.WebApi", Version = "v1" });
+            });
+
+            services.AddDbContext<LibraryContext>(options => {
+                if (Environment.IsDevelopment()) {
+                    options.EnableSensitiveDataLogging().EnableDetailedErrors();
+                }
+                options.UseMySql(
+                    Configuration.GetConnectionString("LibrarySampleDb"),
+                    new MySqlServerVersion("5.7.35"));
             });
         }
 
