cc85a4cfcefc0d713a0c00555eb7ff296bbb631b
netotz <neto.otz@hotmail.com>
2021-08-27 10:01:21 -0500

    Rename property

--- LibrarySample.WebApi/Models/Book.cs
+++ LibrarySample.WebApi/Models/Book.cs
@@ -3,7 +3,7 @@
 namespace LibrarySample.WebApi.Models {
     public class Book {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Title { get; set; }
         public int Pages { get; set; }
         public DateTime PublicationDate { get; set; }
         public int AuthorId { get; set; }
