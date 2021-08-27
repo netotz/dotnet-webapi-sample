26b7944e03fd03f70929648c694d979f5ce82e01
netotz <neto.otz@hotmail.com>
2021-08-26 19:47:55 -0500

    Add database context

---
+++ LibrarySample.WebApi/Database/LibraryContext.cs
@@ -0,0 +1,18 @@
+﻿
+using LibrarySample.WebApi.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace LibrarySample.WebApi.Database {
+    public class LibraryContext : DbContext {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
