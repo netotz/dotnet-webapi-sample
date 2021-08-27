6f56c8b8811b89cb0be8e9c4920c18c37eaf4cfd
netotz <neto.otz@hotmail.com>
2021-08-27 12:26:57 -0500

    Simplify Author name properties

--- LibrarySample.WebApi/Models/Author.cs
+++ LibrarySample.WebApi/Models/Author.cs
@@ -4,9 +4,7 @@ using System.Collections.Generic;
 namespace LibrarySample.WebApi.Models {
     public class Author {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Pseudonym { get; set; }
+        public string Name { get; set; }
         public DateTime BirthDate { get; set; }
         public List<Book> Books { get; set; }
     }
