b3744f6aefd33d5079b174973a3d3ec40c0d0c6a
netotz <neto.otz@hotmail.com>
2021-08-26 19:33:30 -0500

    Add properties for foreign Ids

--- LibrarySample.WebApi/Models/Book.cs
+++ LibrarySample.WebApi/Models/Book.cs
@@ -6,7 +6,9 @@ namespace LibrarySample.WebApi.Models {
         public string Name { get; set; }
         public int Pages { get; set; }
         public DateTime PublicationDate { get; set; }
+        public int AuthorId { get; set; }
         public Author Author { get; set; }
+        public int GenreId { get; set; }
         public Genre Genre { get; set; }
     }
 }
