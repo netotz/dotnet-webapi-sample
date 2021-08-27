59e47a7090690348c844cfe1591ea2a67d186b1f
netotz <neto.otz@hotmail.com>
2021-08-26 19:28:14 -0500

    Add models

---
+++ LibrarySample.WebApi/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LibrarySample.WebApi.Models {
+    public class Book {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Pages { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public Author Author { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
---
+++ LibrarySample.WebApi/Models/Genre.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace LibrarySample.WebApi.Models {
+    public class Genre {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Book> Books { get; set; }
+    }
+}
